640cabccd2b097d2ff2482165328bb48faa9b486
andreonofre <alordandre@gmail.com>
2023-12-13 13:18:39 -0300

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Form } from '../Form'
+
+
+import { 
+    Container,
+    
+ } from './styles' 
+
+
+export function Main () {
+  return (
+    <Container>MAIN
+      <Form />
+    </Container>
+  )
+}
